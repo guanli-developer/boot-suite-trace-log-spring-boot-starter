6b702131640c4ca51305e2a6fe49007079f196be
Li.Guan <guanli_developer@aliyun.com>
2023-01-24 17:56:28 +0800

    fixed configuration bug

--- src/main/java/tech/guanli/boot/log/trace/configuration/TraceLogAutoConfiguration.java
+++ src/main/java/tech/guanli/boot/log/trace/configuration/TraceLogAutoConfiguration.java
@@ -1,12 +1,14 @@
 package tech.guanli.boot.log.trace.configuration;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 import tech.guanli.boot.log.trace.Package;
 
 @AutoConfiguration
 @ComponentScan(basePackageClasses = { Package.class })
+@EnableConfigurationProperties(TraceLogAutoConfigurationProperty.class)
 public class TraceLogAutoConfiguration {
 
 }
